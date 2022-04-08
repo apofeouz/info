755834d4036569c241af2e0f1f3725ec1bbf30ff
apofeouz <60869955+apofeouz@users.noreply.github.com>
2022-04-08 22:38:55 +0300

    Update index.js

--- index.js
+++ index.js
@@ -6,6 +6,9 @@ const translate = require('friendly-node-cron');
 const cron = require('node-cron');
 const { fullRandom, randomImage } = require('random-img-lib');
 const chat_id = '-545281554'
+const axios = require('axios')
+const cheerio = require('cheerio')
+const fs = require('fs')
 
 
 
@@ -15,12 +18,12 @@ bot.hears('Привет', async ctx => {
   });
 
   //Доброе утро
-  cron.schedule(translate('at 09:00:00'), () => {
-    bot.telegram.sendMessage(chat_id, 'Добрый день и удачного дня команда!')
+  cron.schedule(translate('on mon tue wed thu fri sat sun at 09:00:00 every'), () => {
+    bot.telegram.sendMessage(chat_id, 'Добрый день и удачного дня команда! \ud83c\udf1e')
   })
   //Конец дня
-  cron.schedule(translate('at 18:00:00'), () => {
-    bot.telegram.sendMessage(chat_id, 'День окончен! Не забудь завершить работу в Битрикс!')
+  cron.schedule(translate('on mon tue wed thu fri sat sun at 18:00:00'), () => {
+    bot.telegram.sendMessage(chat_id, 'День окончен! Не забудь завершить работу в Битрикс! \ud83d\ude09')
   })
 
 //ФОТО команда
@@ -33,15 +36,6 @@ bot.hears('Фото',  ctx => {
 //    bot.telegram.sendMessage(chat_id, ` ${fullRandom()}`)
 //  })
  
- //Курс дня
- cron.schedule(translate('at 09:00:10'), () => {
- fetch('https://www.cbr-xml-daily.ru/daily_json.js')
-  .then(res => res.json())
-  .then(json => {
-    let date = json.Date.slice(0, 10);
-    ctx.reply(`По данным ЦБР на ${date}, курс USD установлен ${json.Valute.USD.Value.toFixed(2)} и он ${json.Valute.USD.Value < json.Valute.USD.Previous ? ' падает' : ' растёт'}`);
-})
-  });
 
 //Курс Команда
 bot.hears('Курс',  ctx => {
@@ -49,11 +43,21 @@ bot.hears('Курс',  ctx => {
   .then(res => res.json())
   .then(json => {
     let date = json.Date.slice(0, 10);
-    ctx.reply(`По данным ЦБР на ${date}, курс USD установлен ${json.Valute.USD.Value.toFixed(2)} и он ${json.Valute.USD.Value < json.Valute.USD.Previous ? ' падает' : ' растёт'}`);
+    ctx.reply(`По данным ЦБР на ${date}: \ud83c\uddfa\ud83c\uddf8 ${json.Valute.USD.Value.toFixed(2)} ${json.Valute.USD.Value < json.Valute.USD.Previous ? '\ud83d\udd3b' : '\ud83d\udd3a'}`
+    + ` \ud83c\uddea\ud83c\uddfa ${json.Valute.EUR.Value.toFixed(2)} ${json.Valute.EUR.Value < json.Valute.EUR.Previous ? '\ud83d\udd3b' : '\ud83d\udd3a'}`);
   });
   })
-  
 
+  cron.schedule(translate('on mon tue wed thu fri sat sun at 10:00:20'), () => {
+    fetch('https://www.cbr-xml-daily.ru/daily_json.js')
+    .then(res => res.json())
+    .then(json => {
+      let date = json.Date.slice(0, 10);
+      bot.telegram.sendMessage(chat_id, `По данным ЦБР на ${date}: \ud83c\uddfa\ud83c\uddf8 ${json.Valute.USD.Value.toFixed(2)} ${json.Valute.USD.Value < json.Valute.USD.Previous ? '\ud83d\udd3b' : '\ud83d\udd3a'}`
+       + ` \ud83c\uddea\ud83c\uddfa ${json.Valute.EUR.Value.toFixed(2)} ${json.Valute.EUR.Value < json.Valute.EUR.Previous ? '\ud83d\udd3b' : '\ud83d\udd3a'}`);
+    });
+    })
+  
 
 //Погода
 bot.hears('Погода',  ctx => {
@@ -70,7 +74,7 @@ fetch('https://api.weather.yandex.ru/v2/informers?lat=47.23135001&lon=39.7232800
 })
 
 //Погода по времени
-   cron.schedule(translate('at 09:00:15'), () => {
+   cron.schedule(translate('on mon tue wed thu fri sat sun at 09:00:15'), () => {
     fetch('https://api.weather.yandex.ru/v2/informers?lat=47.23135001&lon=39.7232800', {
         method: "GET",
         headers: {
@@ -83,15 +87,73 @@ fetch('https://api.weather.yandex.ru/v2/informers?lat=47.23135001&lon=39.7232800
     })
    })
 
+   //Парсер новостей
+cron.schedule(translate('on 59 minutes'), () => {
+  const startLink = 'https://www.consultant.ru/legalnews/gz/'
+  const getPostTitles = async (link) => {
+    const resp = await axios.get(link)
+    const $ = cheerio.load(resp.data)
+    const postTitles = []
+    $('div.listing-news__list').each((_idx, el) => {
+      postTitles.push({
+      title: $(el).find('div > a.listing-news__item-title').slice(0, 1).text(),
+      link: $(el).find('div > a.listing-news__item-title').slice(0, 1).attr('href'),
+      img: $(el).find('div.listing-news__item-image > img').attr('src'),
+      })
+    })
+    return postTitles
+  }
+  async function testGetPostTitles() {
+    const results = await getPostTitles(startLink)
+    fs.readFile('object.json', (err, data) => {
+      if (err) throw err
+      let results1 = JSON.parse(data)
+      if (results[0].link === results1[0].link) {
+        console.log('Нет новых сообщений')
+      } else {
+        setTimeout( () => fs.writeFile( 'object.json', JSON.stringify(results, null, 4),(err) => 
+        { if (err) {
+                  console.error(err)
+                  return
+                }
+              },
+            ),
+          1000,
+        )
+        bot.telegram.sendMessage(
+          chat_id,
+          results[0].title +
+          '\n' +
+          'Ссылка на новость \ud83d\udc49 https://www.consultant.ru' +
+          results[0].link +
+          '\n',
+        )
+        bot.telegram.sendPhoto(chat_id, results[0].img)
+      }
+    })
+  }
+  testGetPostTitles()
+})
+
+bot.hears('Новость',  ctx => {
+  fs.readFile('object.json', (err, data) => {
+   if (err) throw err
+   let results = JSON.parse(data)
+   ctx.reply(results[0].title + '\n' + 'Ссылка на новость \ud83d\udc49 https://www.consultant.ru' + results[0].link + '\n',)
+   setTimeout( () =>
+   ctx.reply(results[0].img),1000)
+ 
+   });
+ });
 
 //Хелпер
-  bot.help((ctx) => ctx.reply('Список доступных крманд:  \n Напиши боту -> Курс \n Напиши боту -> Погода \n Напиши боту -> Фото \nИспользуй эти команды непосредственно в чате с ботом а не в общем чате'))
+  bot.help((ctx) => ctx.reply('Список доступных крманд:  \n Напиши боту \ud83d\udc49 Курс \n Напиши боту \ud83d\udc49 Погода \n Напиши боту \ud83d\udc49 Фото  \n Напиши боту \ud83d\udc49 Новость \nИспользуй эти команды непосредственно в чате с ботом а не в общем чате'))
 
   bot.on('sticker', ctx => {
     ctx.reply('Прикольный стикер')
   })
-  bot.on('edited_message', ctx => {
-    ctx.reply('Вы успешно изменили сообщение')
+  bot.on('edited_message', async ctx => {
+    await ctx.reply(`${ctx.from.first_name}, вы успешно изменили сообщение`)
   })
  
  bot.launch()
